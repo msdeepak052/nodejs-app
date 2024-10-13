65b41eab3634763faa97852ae47d53858be5c811
msdeepak052 <160315954+msdeepak052@users.noreply.github.com>
2024-10-13 20:56:32 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -4,7 +4,7 @@ sonarqubeScanner({
   serverUrl: 'http://13.201.35.250:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
-	    'sonar.projectName': 'Node JS Application - Sample',
+	    'sonar.projectName': 'Node JS Application',
 	    'sonar.projectKey':'NodeJsDeepak',
 	    'sonar.login': 'squ_229359c90753165ef0e22197dfdc845f6ffd739b',
             'sonar.projectVersion':'1.0',
